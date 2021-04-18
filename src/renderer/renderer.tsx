480d847d7ddd93d0866ba73edf945fe61cd42d68
Abhishek <abhimore74@gmail.com>
2021-04-17 21:54:51 -0400

    live cc

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -11,9 +11,55 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { ArrowRight } from 'react-feather';
 import { desktopCapturer, remote } from 'electron'
+const recorder = require('node-record-lpcm16')
+const speech = require('@google-cloud/speech')
+const client = new speech.SpeechClient();
 
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'en-US';
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: false, // If you want interim results, set this to true
+};
+
+// Create a recognize stream
+const recognizeStream = client
+  .streamingRecognize(request)
+  .on('error',console.error)
+  .on('data', (data: { results: { alternatives: { transcript: any; }[]; }[]; }) =>
+    process.stdout.write(
+      data.results[0] && data.results[0].alternatives[0]
+        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+        : '\n\nReached transcription time limit, press Ctrl+C\n'
+    )
+  );
+
+// Start recording and send the microphone input to the Speech API.
+// Ensure SoX is installed, see https://www.npmjs.com/package/node-record-lpcm16#dependencies
+recorder
+  .record({
+    sampleRateHertz: sampleRateHertz,
+    threshold: 0,
+    // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+    verbose: false,
+    recordProgram: 'sox', // Try also "arecord" or "sox"
+    silence: '10.0',
+  })
+  .stream()
+  .on('error', console.error)
+  .pipe(recognizeStream);
+/*
 async function showSources() {
-
+/*
 navigator.mediaDevices.enumerateDevices()
     .then(devices => {
         let audDevice = devices.filter(device => {
@@ -27,10 +73,10 @@ navigator.mediaDevices.enumerateDevices()
         })
         console.log(audioStream)
     })
-  
   const videoElement = document.querySelector("video")
 
 }
+*/
 
 function CloseButton(): JSX.Element {
   function closeWindow() {
@@ -60,7 +106,7 @@ function AbstractSquare(): JSX.Element {
 
 function GetStartedButton() {
   return (
-      <button type="submit" className="startbutton" onClick={showSources}>
+      <button type="submit" className="startbutton" >
       <span>
       start your session
       </span>
